567de8bb369b4a688d64e0e3046364d591e3d784
Elliot Kang <kkang2097@gmail.com>
2021-11-01 22:14:51 -0700

    Updates/Refactoring
    -installing babel in root directory for "ENOENT: no such file found" error
    -installing Google Cloud vision
    -creating service folder/files to make api.js less bulky

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const services = require('../services/index.js');
+
 /* GET home page. */
 router.get('/users', function(req, res, next) {
   console.log('get "users" route hit');
---
+++ src/server/services/index.js
@@ -0,0 +1,23 @@
+// Google Cloud Imports
+const {Storage} = require('@google-cloud/storage');
+
+const storage = new Storage({
+  projectId: 'csc-847-project-3',
+  keyFilename: '../../hidden.json'
+});
+const bucketName = 'test-847-p3';
+
+
+//Add helper functions inside here.
+//This makes our routes/api.js file less bulky
+module.exports = {
+
+  async uploadToBucket(filePath, fileName) {
+    await storage.bucket(bucketName).upload(filePath, {
+    destination: fileName,
+  });
+
+  console.log(`${filePath} uploaded to ${bucketName}`);
+  }
+
+}
