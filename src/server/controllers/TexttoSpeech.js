785cf5311ca3718b5c5b9a93269fba8cf1580872
Benjamin Kao <benjamin.kao00@gmail.com>
2021-11-04 22:18:58 -0700

    Added Text-to-Speech API connection
    - Added dependencies for Google Cloud's Text-to-Speech API
    - Connected server to Text-to-Speech API
    - Created route that sends back list of available voices
    - Started implementation of route that takes in a text to speech request
      and sends it to the Text-to-Speech API to return an MP3 file.

---
+++ src/server/controllers/TexttoSpeech.js
@@ -0,0 +1,82 @@
+const textToSpeech = require('@google-cloud/text-to-speech');
+const fs = require('fs');
+const util = require('util');
+
+
+const client = new textToSpeech.TextToSpeechClient({
+    keyFilename: "../../hidden.json"
+})
+
+const LanguageCodes = {
+    // Need to figure out issues with language codes and language names
+    // (language names are not descriptive enough so we may just have to pick and choose the languages we want)
+}
+
+const VoiceGender = {
+    FEMALE: "FEMALE",
+    MALE: "MALE",
+    GENERAL: "SSML_VOICE_GENDER_UNSPECIFIED"
+}
+
+
+const Controller = {
+    listLanguages: async () => {
+        const [result] = await client.listVoices();
+        const voices = result.voices;
+
+        console.log('Voices: ');
+        voices.forEach(voice => {
+            console.log(`Name: ${voice.name}`);
+            console.log(`  SSML Voice Gender: ${voice.ssmlGender}`);
+            console.log(`  Natural Sample Rate Hertz: ${voice.naturalSampleRateHertz}`);
+            console.log('  Supported languages:');
+            voice.languageCodes.forEach(languageCode => {
+                console.log(`    ${languageCode}`);
+            });
+        });
+
+        return voices;
+    },
+    makeRequest: async (imageName, text, languageCode, voiceGender) => {
+
+        // Make sure language is supported by Google Text to Speech
+        if(!(languageCode in LanguageCode)) {
+            throw Error({
+                message: `${languageCode} is not a supported Google Text to Speech language.`
+            });
+        }
+
+        // Make sure voiceGender is one of the supported Google Text to Speech ssmlGender options
+        if(!(voiceGender in VoiceGender)) {
+            throw Error({
+                message: `${voiceGender} is not a supported Google Text to Speech voice gender.`
+            });
+        }
+
+
+        const request = {
+            input: {text: text},
+            voice: {languageCode: languageCode, ssmlGender: voiceGender},
+            audioConfig: {audioEncoding: 'MP3'},
+        }
+
+        // Performs the text-to-speech request
+        const [response] = await client.synthesizeSpeech(request);
+
+        // Write the binary audio content to a local file
+
+        // TODO: Set the name of the audio file to be the same name as the picture
+        const writeFile = util.promisify(fs.writeFile);
+        await writeFile(`${imageName}.mp3`, response.audioContent, 'binary');
+        console.log(`Audio content written to file: ${imageName}.mp3`);
+
+
+        // Store the newly created audio file into a Google Cloud Storage Bucket
+
+    }
+
+}
+
+
+
+module.exports = {Controller, VoiceGender, LanguageCodes};
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const TextToSpeechController = require('../controllers/TexttoSpeech').Controller;
+
+
 /* GET home page. */
 router.get('/users', function(req, res, next) {
   console.log('get "users" route hit');
   res.send({ users: ["joe", "bernie", "tulsi", "donald", "bill"] });
 });
+
+
+router.post('/text-to-speech', (req, res, next) => {
+
+  // Get the necessary parameters from the request object
+
+  TextToSpeechController.makeRequest();
+
+});
+
+
+router.get('/text-to-speech/languages', (req, res, next) => {
+  TextToSpeechController.listLanguages()
+    .then((voices) => {
+      res.send({voices: voices});
+    });
+})
+
+
 
 module.exports = router;
